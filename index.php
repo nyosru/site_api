e0544f6a0a5b65f451cd450b5807506ca6e063e4
Sergey <1@php-cat.com>
2024-06-19 03:45:25 +0500

    19

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 
     <link rel="icon" href="https://uralweb.info/9.site/my1807uralweb/download/img/fav.png" type="image/x-icon">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <style>
         .jumbotron {
             xbackground-size: cover;
@@ -25,47 +26,48 @@
 
 <body>
 
-    <main role="main">
-        <section class="jumbotron text-center">
-            <div class="container">
-                <h1 class="jumbotron-heading">API</h1>
-                <p class="lead text-muted">Собрание API интерфейсов для работы сайтов и служб</p>
-                <p>
-                    <a href="https://uralweb.info" target="_blank" class="btn btn-primary my-2">Заказать создание сайта</a>
-                    <a href="https://uralweb.info" target="_blank" class="btn btn-secondary my-2">Записаться на консультацию</a>
-                </p>
-            </div>
-        </section>
-
+<main role="main">
+    <section class="jumbotron text-center">
         <div class="container">
+            <h1 class="jumbotron-heading">API</h1>
+            <p class="lead text-muted">Собрание API интерфейсов для работы сайтов и служб</p>
+            <p>
+                <a href="https://php-cat.com" target="_blank" class="btn btn-primary my-2">Заказать создание сайта</a>
+                <a href="https://php-cat.com" target="_blank" class="btn btn-secondary my-2">Записаться на
+                    консультацию</a>
+            </p>
+        </div>
+    </section>
+
+    <div class="container">
 
 
-            <div class="row">
-                <div class="col-sm-12 col-md-12 col-xl-12">
-                    <h1>Whois</h1>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-xl-12">
+                <h1>Whois</h1>
 
-                    <div id="t1" style='margin-left: 20px; padding-left:20px;border-left: 3px solid rgba(0,255,0,0.2);'>
+                <div id="t1" style='margin-left: 20px; padding-left:20px;border-left: 3px solid rgba(0,255,0,0.2);'>
 
-                        <h3>Доступен домен для реги или нет</h3>
-                        <pre>
+                    <h3>Доступен домен для реги или нет</h3>
+                    <pre>
 <code>
 http://api.php-cat.com/whois.php?domain=stu.com&return=json
 </code>
                             </pre>
 
-                    </div>
                 </div>
             </div>
+        </div>
 
 
-            <div class="row">
-                <div class="col-sm-12 col-md-12 col-xl-12">
-                    <h1>Telegram</h1>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-xl-12">
+                <h1>Telegram</h1>
 
-                    <div id="t1" style='margin-left: 20px; padding-left:20px;border-left: 3px solid rgba(0,255,0,0.2);'>
+                <div id="t1" style='margin-left: 20px; padding-left:20px;border-left: 3px solid rgba(0,255,0,0.2);'>
 
-                        <h3>шлём сообщение кому либо подписанному на бота @UralwebBot</h3>
-                        <pre>
+                    <h3>шлём сообщение кому либо подписанному на бота @UralwebBot</h3>
+                    <pre>
 <code>
     file_get_contents('https://api.uralweb.info/telegram.php?' . http_build_query(
         array(
@@ -76,7 +78,7 @@ http://api.php-cat.com/whois.php?domain=stu.com&return=json
     ));
 </code>
                             </pre>
-                        <pre>
+                    <pre>
 <code>
 
 const sendToTelegramm = async (msg) => {
@@ -116,8 +118,8 @@ const sendToTelegramm = async (msg) => {
 }
 </code>
                             </pre>
-                        <h3>шлём тех оповещение в бот @UralwebBot</h3>
-                        <pre>
+                    <h3>шлём тех оповещение в бот @UralwebBot</h3>
+                    <pre>
 <code>
     file_get_contents('https://api.uralweb.info/telegram.php?' . http_build_query(
         array(
@@ -128,17 +130,17 @@ const sendToTelegramm = async (msg) => {
 </code>
                             </pre>
 
-                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-sm-12 col-md-12 col-xl-12">
-                    <h1>VK</h1>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-xl-12">
+                <h1>VK</h1>
 
-                    <div id="t1" style='margin-left: 20px; padding-left:20px;border-left: 3px solid rgba(0,255,0,0.2);'>
+                <div id="t1" style='margin-left: 20px; padding-left:20px;border-left: 3px solid rgba(0,255,0,0.2);'>
 
-                        <h3>отправка сообщения подписанному от группы</h3>
-                        <pre>
+                    <h3>отправка сообщения подписанному от группы</h3>
+                    <pre>
 <code>
     file_get_contents('https://api.uralweb.info/vk.php?' . http_build_query(
         array(
@@ -151,21 +153,22 @@ const sendToTelegramm = async (msg) => {
 </code>
                             </pre>
 
-                    </div>
                 </div>
             </div>
         </div>
+    </div>
 
-    </main>
+</main>
 
-    <footer class="text-muted" style="margin-top:3em; padding-top:3em; padding-bottom:3em; border-top: 3px solid rgba(0,0,0,0.1);">
-        <div class="container">
-            <p class="float-right">
-                Все права защищены
-            </p>
-            <p>Можете копировать всякое с сайта и публиковать везде где получится</p>
-        </div>
-    </footer>
+<footer class="text-muted"
+        style="margin-top:3em; padding-top:3em; padding-bottom:3em; border-top: 3px solid rgba(0,0,0,0.1);">
+    <div class="container">
+        <p class="float-right">
+            Все права защищены
+        </p>
+        <p>Можете копировать всякое с сайта и публиковать везде где получится</p>
+    </div>
+</footer>
 
 </body>
 
