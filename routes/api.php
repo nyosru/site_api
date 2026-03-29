d9cccf33611ca592366d50e8e3f2a11676300f86
nyosru <nyos@rambler.ru>
2026-03-29 06:05:00 +0500

    ++ get msg from telegram bot

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,21 @@ use App\Http\Controllers\Api\HealthController;
 use App\Http\Controllers\Api\TelegramController;
 use App\Http\Controllers\Api\TelegramWebhookController;
 use App\Http\Controllers\Api\WhoisController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/health', HealthController::class);
 Route::get('/whois', WhoisController::class);
 Route::any('/telegram', TelegramController::class);
 Route::any('/telegram/webhook', TelegramWebhookController::class);
+
+$text = 'api';
+
+Log::info('telegram webhook received', [
+    'update_id' => $payload['update_id'] ?? null,
+    'telegram_user_id' => $from['id'] ?? null,
+    'username' => $from['username'] ?? null,
+    'command' => $command ?? 11,
+    'text' => $text !== '' ? $text : null,
+]);
+
